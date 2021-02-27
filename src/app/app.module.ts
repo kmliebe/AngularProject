d9958637357da940975739a6afb0e4bd08aed5de
Katie <kmliebe47@gmail.com>
2021-02-26 23:45:43 -0500

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
+import { DishService } from './services/dish.service';
 
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
@@ -35,7 +36,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule
 
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
